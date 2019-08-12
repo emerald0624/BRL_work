0765833e39d699f2208965673764536e4086a424
Jungkeuk Kim <38102943+emerald0624@users.noreply.github.com>
2019-08-12 18:50:53 +0900

    Add files via upload

---
+++ sensor_read_MCP3208_v3/sensor_read_MCP3208_v3.ino
@@ -0,0 +1,130 @@
+#define SELPIN1 10 //Selection Pin 
+#define SELPIN2 11 //Selection Pin 
+#define DATAOUT 75//MOSI 
+#define DATAIN 74//MISO 
+#define SPICLOCK 76//Clock 
+int readvalue;
+const int R0 = 500; // Trust Resistance value
+const float factor[11] = {134.7277965, 187.0270571, 155.7659395, 218.3626418, 157.1815681, 164.3437955, 145.1582451, 167.1407023, 134.5570137, 223.7073385, 120};
+
+String str = "";
+
+int t1;
+int t2;
+int n_test = 20;
+const int imax = 12;
+unsigned int value[imax];
+float resist[imax - 1];
+float resist0[imax - 1];
+float force[imax - 1];
+void setup() {
+  //set pin modes
+  pinMode(SELPIN1, OUTPUT);
+  pinMode(SELPIN2, OUTPUT);
+  pinMode(DATAOUT, OUTPUT);
+  pinMode(DATAIN, INPUT);
+  pinMode(SPICLOCK, OUTPUT);
+  //disable device to start with
+  digitalWrite(SELPIN1, HIGH);
+  digitalWrite(SELPIN2, HIGH);
+  digitalWrite(DATAOUT, LOW);
+  digitalWrite(SPICLOCK, LOW);
+
+  Serial.begin(115200);
+  for (int i = 0; i < imax; i++) {
+    resist0[i] = 0;
+  }
+
+
+  for (int ii = 0; ii < n_test; ii++) {
+    for (int i = 0; i < imax; i++) {
+      if (i < 8) {
+        digitalWrite(SELPIN1, LOW);
+        value[i] = read_adc(i);
+        digitalWrite(SELPIN1, HIGH);
+      }
+      else {
+        digitalWrite(SELPIN2, LOW);
+        value[i] = read_adc(i - 8);
+        digitalWrite(SELPIN2, HIGH);
+
+      }
+      //delay(10);
+    }
+    for (int i = 1; i < imax; i++) {
+      resist0[i - 1] = resist0[i - 1] + R0 * float(value[i]) / float(value[0] - value[i]) / float(n_test);
+    }
+  }
+
+  t1 = micros();
+}
+
+
+int read_adc(int channel) {
+  int adcvalue = 0;
+  byte commandbits = B11000000; //command bits - start, mode, chn (3), dont care (3)
+
+  //allow channel selection
+  commandbits |= ((channel) << 3);
+
+  //digitalWrite(selpin, LOW); //Select adc
+  // setup bits to be written
+  for (int i = 7; i >= 3; i--) {
+    digitalWrite(DATAOUT, commandbits & 1 << i);
+    //cycle clock
+    digitalWrite(SPICLOCK, HIGH);
+    digitalWrite(SPICLOCK, LOW);
+  }
+
+  digitalWrite(SPICLOCK, HIGH);   //ignores 2 null bits
+  digitalWrite(SPICLOCK, LOW);
+  digitalWrite(SPICLOCK, HIGH);
+  digitalWrite(SPICLOCK, LOW);
+
+  //read bits from adc
+  for (int i = 11; i >= 0; i--) {
+    adcvalue += digitalRead(DATAIN) << i;
+    //cycle clock
+    digitalWrite(SPICLOCK, HIGH);
+    digitalWrite(SPICLOCK, LOW);
+  }
+  //Serial.println(adcvalue);
+  //digitalWrite(selpin, HIGH); //turn off device
+  return adcvalue;
+}
+
+
+void loop() {
+  for (int i = 0; i < imax; i++) {
+    if (i < 8) {
+      digitalWrite(SELPIN1, LOW);
+      value[i] = read_adc(i);
+      digitalWrite(SELPIN1, HIGH);
+
+    }
+    else {
+      digitalWrite(SELPIN2, LOW);
+      value[i] = read_adc(i - 8);
+      digitalWrite(SELPIN2, HIGH);
+
+    }
+    //delay(10);
+  }
+  for (int i = 1; i < imax; i++) {
+    resist[i - 1] = R0 * float(value[i]) / float(value[0] - value[i]);
+    force[i - 1] = factor[i - 1] * (1 / resist[i - 1] - 1 / resist0[i - 1]);
+    //amp[i-1] = 0.1/resist[i-1];
+  }
+  str = "";
+//  for (int i = 1 ; i < imax ; i++) {
+//    str = str + String(force[i - 1]) + ' ';
+//  }
+  //  delay(10);
+  //    t2 = micros();
+  //    int delt = 1000000 / (t2 - t1);
+  //    t1 = t2;
+    Serial.print(force[0]+force[1]+force[2]);
+    Serial.print(',');
+    Serial.println(force[10]);
+//  Serial.println(str);
+}
